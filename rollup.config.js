f93bb00c68fbd2e9e8e40f9661825d4dcec3b851
Justin Beaudry <beaudry.justin@gmail.com>
2018-05-28 19:35:47 -0700

    refactors to use import syntax and adds browser bundle

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import eslint from 'rollup-plugin-eslint';
+import json from 'rollup-plugin-json';
+import builtins from 'rollup-plugin-node-builtins';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  input: './lib/enjoi.js',
+  output: {
+    name: 'Enjoi',
+    file: 'dist/enjoi.min.js',
+    format: 'iife'
+  },
+  plugins: [
+    builtins(),
+    nodeResolve({
+      main: false,
+      browser: true,
+    }),
+    commonjs({
+      sourceMap: true
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      presets: [
+        [
+          "env",
+          {
+            modules: false
+          }
+        ]
+      ],
+      plugins: [
+        "external-helpers"
+      ]
+    }),
+    json(),
+    eslint({
+      fix: true
+    }),
+//    uglify()
+  ]
+};
